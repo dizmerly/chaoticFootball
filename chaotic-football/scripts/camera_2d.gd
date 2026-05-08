a5b46c1e2871a975d6ebd34f7bed6799d4d271f4
Daniel Izmerly <dan.izmerly@gmail.com>
2026-05-08 11:29:05 -0500

    added dynamic camera

--- chaotic-football/scripts/camera_2d.gd
+++ chaotic-football/scripts/camera_2d.gd
@@ -1,8 +1,27 @@
 extends Camera2D
 
+
+@onready var player = $"../Player"
+@onready var ball = $"../Ball"
+
+const MIN_ZOOM = 1.5
+const MAX_ZOOM = 4.0 
+const ZOOM_DISTANCE = 400.0 
+const MARGIN = 1.05
+var margin = 20 #px
+
+
 func _ready() -> void:
 	pass
 
-func _process(delta: float) -> void:
-	position = position.lerp($"../Player".position, delta * 3)
+func _physics_process(delta: float) -> void:
+	var dist = ball.global_position.distance_to(player.global_position) * MARGIN
+	var t = clamp(dist/ZOOM_DISTANCE, 0.0, 1.0)
+	t = pow(t, 2)
+	var target_zoom = lerp(MAX_ZOOM, MIN_ZOOM, t)
+	zoom = zoom.lerp(Vector2(target_zoom, target_zoom), 5 * delta)
+	
+	
+	limit_left = min(player.global_position.x, ball.global_position.x) - margin
+	limit_right = max(player.global_position.x, ball.global_position.x) + margin
 	
--- chaotic-football/scripts/character_body_2d.gd
+++ chaotic-football/scripts/character_body_2d.gd
@@ -22,10 +22,16 @@ func UpdateAnimation() -> void:
 
 func _physics_process(delta: float) -> void:
 	UpdateAnimation()
+	var direction := Input.get_axis("left", "right")
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
+#	handle whether character is going left or right
+	sprite.flip_h = direction < 0
+	
+#	State machine in order to handle different actions
 	match current_state:
 		States.IDLE:
 			print("IDLE")
@@ -43,13 +49,13 @@ func _physics_process(delta: float) -> void:
 			print("JUMPING")
 			if is_on_floor():
 				current_state = States.IDLE
+				
 	# Handle jump.
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
 	
 	
 	if direction:
