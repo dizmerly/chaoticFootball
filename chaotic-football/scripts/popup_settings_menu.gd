f8d0f2e0320f2b893ba43a26a3774384ac09c822
Daniel Izmerly <dan.izmerly@gmail.com>
2026-08-06 16:34:23 -0500

    setting smenu highlighting when focused, focus for popup menu is automatically retrieved, you can open and close different parts of the menu easily

--- chaotic-football/scripts/popup_settings_menu.gd
+++ chaotic-football/scripts/popup_settings_menu.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var settings_menu: Control = $"Settings Menu"
 @onready var popup_panel: Panel = $Panel
  
+@onready var resume_button: Button = $Panel/VBoxContainer/resume_button
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,11 +18,15 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("open_settings"):
 		if popup_panel.visible:
 			popup_panel.hide()
+			get_viewport().gui_release_focus()
+			get_tree().paused = false
 		elif settings_menu.visible:
 			settings_menu.hide()
 			popup_panel.show()
+			resume_button.grab_focus()
 		else:
 			popup_panel.show()
+			resume_button.grab_focus()
 			
 #		TODO: add support for moving around the menus with dpad
 
