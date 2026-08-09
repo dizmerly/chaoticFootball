0476a3cfc521fa027536be573e323fe82e9fc382
Daniel Izmerly <dan.izmerly@gmail.com>
2026-08-08 23:37:41 -0500

    Add character definitions to selection panel

--- chaotic-football/scripts/character_selection_panel.gd
+++ chaotic-football/scripts/character_selection_panel.gd
@@ -2,6 +2,17 @@ extends Control
 
 @onready var panel: Panel = $Panel
 
+const CHARACTERS = [
+	{
+		"id": "spain",
+		"portrait": preload("res://assets/SoccerGame/playerTwo.png")
+	},
+	{
+		"id": "argentina",
+		"portrait": preload("res://assets/SoccerGame/playerOne.png")
+	}
+]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
