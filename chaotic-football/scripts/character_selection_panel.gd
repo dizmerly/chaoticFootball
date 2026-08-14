95871640b79485b2a1299b87ebedde443571fdb7
Daniel Izmerly <dan.izmerly@gmail.com>
2026-08-14 15:26:03 -0500

    Add controller-based character selection and ready states

--- chaotic-football/scripts/character_selection_panel.gd
+++ chaotic-football/scripts/character_selection_panel.gd
@@ -7,25 +7,28 @@ extends Control
 @onready var red_image: TextureRect = $red_portrait/red_image
 
 #UI Buttons
+var blue_index: int = 0
+var red_index: int = 0
 
+#UI Info
+@onready var ready_red: Button = $ReadyRed
+@onready var ready_blue: Button = $ReadyBlue
 
-
-var blue_index: int
-var red_index: int
-
-
+# Signals 
+# TODO: either here or in game manger, once all controllers have
+# emitted that they have selected a character, load the game.
 signal character_selected(controller_id: int, spritesheet_path: String)
 
 
 const CHARACTERS = [
 	{
 		"id": "spain",
-		"portrait": preload("res://assets/SoccerGame/spainSpritesheet.png"),
+		"portrait": preload("res://assets/SoccerGame/portraits/spainPortrait.png"),
 		"spritesheet": "res://assets/SoccerGame/spainSpritesheet.png"
 	},
 	{
 		"id": "argentina",
-		"portrait": preload("res://assets/SoccerGame/argentinaSpritesheet.png"),
+		"portrait": preload("res://assets/SoccerGame/portraits/argentinaPortrait.png"),
 		"spritesheet" : "res://assets/SoccerGame/argentinaSpritesheet.png"
 	}
 ]
@@ -35,6 +38,11 @@ func _ready() -> void:
 	#eventually preferably store these as settings for user preferences
 	blue_index = 0
 	red_index = 0
+	ready_red.hide()
+	ready_blue.hide()
+	
+	load_image(red_index, "red")
+	load_image(blue_index, "blue")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -42,7 +50,7 @@ func _process(delta: float) -> void:
 	pass
 
 func load_image(image_index: int, team_color: String) -> void:
-	if image_index < 0 or image_index > CHARACTERS.size():
+	if image_index < 0 or image_index >= CHARACTERS.size():
 		push_error("Invalid character index, %d" % image_index)
 		return
 	var character = CHARACTERS[image_index]
@@ -67,9 +75,50 @@ func confirm_selection(controller_id: int, image_index: int) -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventJoypadButton:
-		if event.button_index == JOY_BUTTON_A and event.pressed:
-			var controller_id := event.device
-			if controller_id == 0:
+		var controller_id = event.device
+		
+#		Confirm ready
+		if event.button_index == JOY_BUTTON_A and event.is_pressed():
+			if controller_id in GameManager.blue_team:
 				confirm_selection(controller_id, blue_index)
-			elif controller_id == 1:
+				ready_blue.show()
+			elif controller_id == GameManager.red_team:
 				confirm_selection(controller_id, red_index)
+				ready_red.show()
+#		Cycle left and right
+		if event.button_index == JOY_BUTTON_DPAD_LEFT and event.is_pressed():
+			if controller_id in GameManager.blue_team:
+				cycle_blue_left()
+			elif controller_id in GameManager.red_team:
+				cycle_red_left()
+		if event.button_index == JOY_BUTTON_DPAD_RIGHT and event.is_pressed():
+			if controller_id in GameManager.blue_team:
+				cycle_blue_right()
+			elif controller_id in GameManager.red_team:
+				cycle_red_right()
+#		Cancel Ready
+		if event.button_index == JOY_BUTTON_B and event.is_pressed():
+			if controller_id in GameManager.blue_team:
+				ready_blue.hide()
+			if controller_id in GameManager.red_team:
+				ready_red.hide()
+
+func cycle_blue_left() -> void: 
+	if ready_blue.hidden:
+		blue_index = wrapi(blue_index - 1, 0, CHARACTERS.size())
+		load_image(blue_index, "blue")
+				
+func cycle_blue_right() -> void: 
+	if ready_blue.hidden:
+		blue_index = wrapi(blue_index + 1, 0, CHARACTERS.size())
+		load_image(blue_index, "blue")
+
+func cycle_red_left() -> void:
+	if ready_red.hidden: 
+		red_index = wrapi(red_index - 1, 0, CHARACTERS.size())
+		load_image(red_index, "red")
+
+func cycle_red_right() -> void: 
+	if ready_red.hidden:	
+		red_index = wrapi(red_index + 1, 0, CHARACTERS.size())
+		load_image(red_index, "red")
