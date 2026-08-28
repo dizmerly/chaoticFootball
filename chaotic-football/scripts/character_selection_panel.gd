6f83ad2864de126f1eefd255b3ef414cd25ddb60
Daniel Izmerly <dan.izmerly@gmail.com>
2026-08-28 08:38:06 -0500

    Restore character selection back button after branch switching

--- chaotic-football/scripts/character_selection_panel.gd
+++ chaotic-football/scripts/character_selection_panel.gd
@@ -150,3 +150,7 @@ func _on_right_blue_pressed() -> void:
 
 func _on_left_blue_pressed() -> void:
 	cycle_blue_left()
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
